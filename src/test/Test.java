624e0420bb7cabe5f63c74ed2e694741be0a9536
Tanimura <tanimura@ncs.ac.jp>
2021-06-18 10:28:49 +0900

    追加1

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,8 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		String s = "こんばんは！";
 		System.out.println("こんにちは！");
-
+		System.out.println(s);
 	}
 
 }
